3d765ab4018634a3d9de17b41b3063a992653b4a
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-11-20 19:22:53 +0300

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -1,20 +1,21 @@
+import java.util.Date;
+
 public class Block{
 
     public static void main(String[] args) {
-        Block bChain = new Block();
-        Block a = new Block("1",new java.util.Date(),"defne","rumeysa",260);
-        Block b = new Block("2",new java.util.Date(),"rumeysa","ozgur",680);
-        Block c = new Block("3",new java.util.Date(),"ozgur","defne",5000);
-        Block d = new Block("4",new java.util.Date(),"ozgur","rumeysa",4200);
-        Block e = new Block("5",new java.util.Date(),"rumeysa","defne",899);
+        //Transaction bChain = new Transaction();
+        Transaction a = new Transaction("1",new Date(),"defne","rumeysa",260);
+        Transaction b = new Transaction("2",new java.util.Date(),"rumeysa","ozgur",680);
+        Transaction c = new Transaction("3",new java.util.Date(),"ozgur","defne",5000);
+        Transaction d = new Transaction("4",new java.util.Date(),"ozgur","rumeysa",4200);
+        Transaction e= new Transaction("5",new java.util.Date(),"rumeysa","defne",899);
 
-        bChain.addBlock(a);
-        bChain.addBlock(b);
-        bChain.addBlock(c);
-        bChain.addBlock(d);
-        bChain.addBlock(e);
+        Blockchain.add(a);
+        Blockchain.add(b);
+        Blockchain.add(c);
+        Blockchain.add(d);
+        Blockchain.add(e);
 
-        bChain.printChain();
+        Blockchain.printChain();
     }
-
 }
