275c7e5adee6eb91e44debe77f0848d4d426d5c0
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-11-20 17:52:38 +0300

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,22 +1,62 @@
+import java.util.Date;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.security.*;
 public class Transaction {
-    private String sourceAddress;
-    private String targetAddress;
+    private String previousHash;
+    private String hash;
+    private String version;
+    private Date time;
+    private String sourceName;
+    private String targetName;
     private int amount;
 
-    public String getSourceAddress() {
-        return sourceAddress;
+    public String getPreviousHash() {
+        return previousHash;
     }
 
-    public void setSourceAddress(String sourceAddress) {
-        this.sourceAddress = sourceAddress;
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
     }
 
-    public String getTargetAddress() {
-        return targetAddress;
+    public String getHash() {
+        return hash;
     }
 
-    public void setTargetAddress(String targetAddress) {
-        this.targetAddress= targetAddress;
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getSourcename() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getTargetName() {
+        return targetName;
+    }
+
+    public void setTargetName(String targetName) {
+        this.targetName = targetName;
     }
 
     public int getAmount() {
@@ -27,9 +67,30 @@ public class Transaction {
         this.amount = amount;
     }
 
-    public Transaction(String sourceAddress, String targetAddress, int amount) {
-        this.sourceAddress = sourceAddress;
-        this.targetAddress = targetAddress;
+    public Transaction(String version, Date time, String sourceName, String targetName, int amount) {
+        this.version = version;
+        this.time = time;
+        this.sourceName = sourceName;
+        this.targetName = targetName;
         this.amount = amount;
+        this.hash = decodeHash();
     }
+
+    public String decodeHash() {
+        String dataToHash = "" + this.version + this.time + this.previousHash + this.amount;
+
+        MessageDigest digest;
+        String encoding = null;
+
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(dataToHash.getBytes(StandardCharsets.UTF_8));
+            encoding = Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        this.hash = encoding;
+        return encoding;
+    }
+
 }
